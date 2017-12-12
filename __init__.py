798b43eae9175c9c1bdaf937c23cff86daee10cb
Geoff Crompton <geoffc@trinity.unimelb.edu.au>
2017-12-12 12:10:52 +1100

    Further work.
    With this change I might now be handling authentication effectively.
    Hopefully with the cookie_jar variable intialised we'll avoid having to
    login to salt every time there is a message.

--- __init__.py
+++ __init__.py
@@ -3,42 +3,58 @@ import aiohttp
 import logging
 
 
-logged_in = False
-token = None
+class LoginError(Exception):
+    pass
 
 async def login(config, session):
     url = config['url']
-    username = config['username']
-    password = config['password']
     data = {
-        'username': username,
-        'password': password,
-        'eauth': 'auto',
+        'username': config['username'],
+        'password': config['password'],
+        'eauth': config['eauth'],
     }
     async with session.post(url + '/login', json=data) as resp:
-        # TODO: onsider response code, handle errors.
-        logged_in = True
+        if resp.status != 200:
+            logging.warning(
+                "Login to {} failed, status={}. username={}, eauth={}".format(
+                resp.request_info.url, resp.status, config['username'],
+                config['eauth']))
+            raise LoginError
+
+
+cookie_jar = None
+# Setup a cookie jar we can re-use between requests, in order to save
+# logging in repeatedly.
+def setup(config):
+    cookie_jar = aiohttp.CookieJar()
 
 async def run_post(runner, config):
     verify_ssl = True
     if 'verify_ssl' in config:
         verify_ssl = config['verify_ssl']
     conn = aiohttp.TCPConnector(verify_ssl=verify_ssl)
-    async with aiohttp.ClientSession(connector=conn) as session:
-        if not logged_in:
+    async with aiohttp.ClientSession(connector=conn, cookie_jar=cookie_jar) as session:
+        if len([i for i in session.cookie_jar if i.key == 'session_id']) == 0:
             await login(config, session)
 
         data = {
             'client': 'runner',
             'fun': runner
         }
-        async with session.post(url, data) as resp:
+        url = config['url']
+        async with session.post(url, json=data) as resp:
             return resp
 
 @match_regex(r'^salt-run (.*)')
 async def salt_run(opsdroid, config, message):
     runner = message.regex.group(1)
 
-    result = await run_post(runner, config)
-    await message.respond(result.json())
-
+    try:
+        result = await run_post(runner, config)
+        if result.status != 200:
+            await message.respond('Error returned from salt for runner {}: {} ({})'.format(runner, result.status, result.reason))
+        else:
+            result_text = await result.text()
+            await message.respond(result_text)
+    except LoginError:
+        await message.respond('Login Error to salt. Check logs.')
